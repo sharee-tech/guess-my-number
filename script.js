7a13af4fe43a2a33d41f32c2d94088a0e9942975
sharee-tech <mail.sharee@gmail.com>
2024-03-09 16:05:09 -0600

    refactored code to clean code

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict";
 
-// console.log(document.querySelector(".message").textContent);
-// document.querySelector(".message").textContent = "🎉 Correct Number!";
-// console.log(document.querySelector(".message").textContent);
-
-// document.querySelector(".number").textContent = 13;
-// document.querySelector(".score").textContent = 10;
-
-// document.querySelector(".guess").value = 23;
-// console.log(document.querySelector(".guess").value);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
@@ -28,18 +18,10 @@ document.querySelector(".check").addEventListener("click", function() {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too High!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💥 You lost the game!";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too Low";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "📈 Too High!" : "📉 Too Low";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
