e98245e1b1e57b6e25f72f4ccedfd3a2aa1a126e
sharee-tech <mail.sharee@gmail.com>
2024-03-09 15:52:37 -0600

    implemented highschore functionality

--- script.js
+++ script.js
@@ -11,8 +11,8 @@
 // console.log(document.querySelector(".guess").value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function() {
   const guess = Number(document.querySelector(".guess").value);
@@ -23,6 +23,11 @@ document.querySelector(".check").addEventListener("click", function() {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too High!";
@@ -47,6 +52,7 @@ document.querySelector(".check").addEventListener("click", function() {
 document.querySelector(".again").addEventListener("click", function() {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = "?";
   document.querySelector(".score").textContent = score;
   document.querySelector(".guess").value = "";
   document.querySelector(".message").textContent = "Start guessing...";
