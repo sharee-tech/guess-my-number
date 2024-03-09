d07698d2c87843b8e222a6fb5c723474dbeb5e81
sharee-tech <mail.sharee@gmail.com>
2024-03-09 15:40:37 -0600

    added reset functionality

--- script.js
+++ script.js
@@ -11,17 +11,18 @@
 // console.log(document.querySelector(".guess").value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function() {
   const guess = Number(document.querySelector(".guess").value);
-
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No Number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too High!";
@@ -41,4 +42,13 @@ document.querySelector(".check").addEventListener("click", function() {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function() {
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = null;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
